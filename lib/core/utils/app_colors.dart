e73551ae99d274b221f4429097ae4bce337f2d99
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-01-14 13:24:35 +0200

    add app color files

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const Color mainColor = Color(0x00003366);
+  static const Color secondaryColor = Color(0x00FF6600);
+}
