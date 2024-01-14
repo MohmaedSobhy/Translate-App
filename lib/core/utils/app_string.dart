0cb0f4bc9ef3e0db3e93507b08cd65ce0f9f498c
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-01-14 13:18:01 +0200

    app string remove from and to words

--- lib/core/utils/app_string.dart
+++ lib/core/utils/app_string.dart
@@ -1,7 +1,6 @@
 abstract class AppString {
-  static const String appName = "Translator";
-  static const String from = "From";
-  static const String to = "To";
+  static const String appName = "language Translator";
+  static const String from = "Translate";
 
   static const List<String> languages = [
     'English',
--- lib/translate_app.dart
+++ lib/translate_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:translate_app/core/utils/app_string.dart';
 
 class TranslateApp extends StatelessWidget {
   const TranslateApp({super.key});
@@ -7,7 +8,7 @@ class TranslateApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Translate App',
+      title: AppString.appName,
       theme: ThemeData(
         useMaterial3: false,
       ),
