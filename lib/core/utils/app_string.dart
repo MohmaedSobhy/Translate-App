7f91d7f3c53c3f7c9457f1886b864e32e133ae6f
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-01-14 13:10:01 +0200

    add file app string

---
+++ lib/core/utils/app_string.dart
@@ -0,0 +1,28 @@
+abstract class AppString {
+  static const String appName = "Translator";
+  static const String from = "From";
+  static const String to = "To";
+
+  static const List<String> languages = [
+    'English',
+    'Hindi',
+    'Arabic	',
+    'German',
+    'Russian',
+    'Spanish',
+    'Urdu',
+    'Japanese	',
+    'Italian'
+  ];
+  static const List<String> languageCode = [
+    'en',
+    'hi',
+    'ar',
+    'de',
+    'ru',
+    'es',
+    'ur',
+    'ja',
+    'it'
+  ];
+}
