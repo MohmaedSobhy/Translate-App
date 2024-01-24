a32a69d8d82252b1f301d46476585eac9f66ca25
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-01-24 19:57:25 +0200

    create file app assest

---
+++ lib/core/utils/app_assest.dart
@@ -0,0 +1,6 @@
+abstract class AppAssests {
+  static const String base = "images/";
+  static const String appLogo = "${base}logo.png";
+  static const String leftEllipse = "${base}ellipse_1.jpg";
+  static const String rightEllipse = "${base}ellipse_2.jpg";
+}
