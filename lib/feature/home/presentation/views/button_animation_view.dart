78f5da6a00501dd96d5832bdcc31e319d2dd9497
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-01-26 20:47:55 +0200

    button animation

---
+++ lib/feature/home/presentation/views/button_animation_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ButtonView extends StatelessWidget {
+  const ButtonView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(),
+      child: const Row(
+        children: [],
+      ),
+    );
+  }
+}
