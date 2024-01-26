68d6715fac8841629454df664a323ba6dcc0537f
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-01-26 22:31:08 +0200

    add purple color

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -4,5 +4,5 @@ abstract class AppColor {
   static const Color mainColor = Color(0xf0003366);
   static const Color secondaryColor = Color(0xf0FF6600);
   static const Color black = Color(0xf0000000);
-  static const Color gray = Color(0xf0FFFBFE);
+  static const Color purple = Color(0xf06750A4);
 }
