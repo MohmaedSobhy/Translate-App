01ac9c4872e9babe6db63fe5de85fa01da706d86
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-01-26 22:30:27 +0200

    add button animation view

--- lib/feature/home/presentation/views/button_animation_view.dart
+++ lib/feature/home/presentation/views/button_animation_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:translate_app/core/utils/app_colors.dart';
+import 'package:translate_app/feature/home/presentation/widgets/country_widget.dart';
 
 class ButtonView extends StatelessWidget {
   const ButtonView({super.key});
@@ -6,9 +8,33 @@ class ButtonView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(),
-      child: const Row(
-        children: [],
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: AppColor.purple.withOpacity(0.09),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const CountryWidget(
+            languageName: 'English',
+            imagePath: 'images/code.png',
+            leftDirection: true,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.compare_arrows,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
+          const CountryWidget(
+            languageName: 'Spanish',
+            imagePath: 'images/code.png',
+            leftDirection: false,
+          ),
+        ],
       ),
     );
   }
