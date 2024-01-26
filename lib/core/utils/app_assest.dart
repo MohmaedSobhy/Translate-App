3f1ca604d6585fd0f438b39ab81829b3af793f0c
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-01-26 22:32:29 +0200

    remove elipse images path

--- lib/core/utils/app_assest.dart
+++ lib/core/utils/app_assest.dart
@@ -1,6 +1,4 @@
 abstract class AppAssests {
   static const String base = "images/";
   static const String appLogo = "${base}logo.png";
-  static const String leftEllipse = "${base}ellipse_1.jpg";
-  static const String rightEllipse = "${base}ellipse_2.jpg";
 }
