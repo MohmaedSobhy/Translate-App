7df51678cfe04c2ed6b577b2a4f766b6d66fbadd
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-02-16 10:11:20 +0200

    add variable assest path for every flage

--- lib/core/utils/app_assest.dart
+++ lib/core/utils/app_assest.dart
@@ -1,4 +1,12 @@
 abstract class AppAssests {
   static const String base = "images/";
   static const String appLogo = "${base}logo.png";
+  static const String egyptFlag = "${base}egypt.png";
+  static const String turkeyFlage = "${base}turkey.png";
+  static const String unitedKindom = "${base}united_kingdom.png";
+  static const String argentine = "${base}argentine.png";
+  static const String unitedStates = "${base}united_states.png";
+  static const String indian = "${base}indian.png";
+  static const String russia = "${base}russia.png";
+  static const String germany = "${base}germany.png";
 }
