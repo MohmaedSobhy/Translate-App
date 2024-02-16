c6c33ac6634e14005b661165bb2d1e8455e01171
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-02-16 09:32:39 +0200

    desgin app bar for select language screen

--- lib/feature/home/presentation/screens/select_language_screen.dart
+++ lib/feature/home/presentation/screens/select_language_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:translate_app/core/utils/app_string.dart';
 
 class SelectLanguageScreen extends StatelessWidget {
   const SelectLanguageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          AppString.selectLanguage,
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).canPop();
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
   }
 }
