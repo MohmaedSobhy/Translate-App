5a1d248ed93303fa8ace7da0c53083528d89f487
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-02-16 09:29:51 +0200

    add variable to select language

--- lib/core/utils/app_string.dart
+++ lib/core/utils/app_string.dart
@@ -2,4 +2,5 @@ abstract class AppString {
   static const String appName = "Language Translator";
   static const String from = "Translate";
   static const String translateOnToGo = "Translate on the Go";
+  static const String selectLanguage = 'Select Language';
 }
