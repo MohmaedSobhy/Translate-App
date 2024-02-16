38d504760c7a384ba330019cae18f7edf9748890
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-02-16 19:06:01 +0200

    list view of language

--- lib/feature/home/presentation/views/select_language_body_view.dart
+++ lib/feature/home/presentation/views/select_language_body_view.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:translate_app/feature/home/data/model/language.dart';
+import 'package:translate_app/feature/home/presentation/views/select_language_item.dart';
 
 class SelectLanguageBodyView extends StatelessWidget {
   const SelectLanguageBodyView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width * 0.02,
+      ),
+      child: ListView.separated(
+        itemCount: LanguageModel.availableLanguage.length,
+        separatorBuilder: (context, index) {
+          return const Divider(
+            color: Colors.grey,
+          );
+        },
+        itemBuilder: (context, index) {
+          return SelectLanguageItem(
+            language: LanguageModel.availableLanguage[index],
+          );
+        },
+      ),
+    );
   }
 }
