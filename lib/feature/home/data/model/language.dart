9bc8aa72e0f811ca010da96be5d5727b83333915
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-02-16 09:58:26 +0200

    create language class

--- lib/feature/home/data/model/language.dart
+++ lib/feature/home/data/model/language.dart
@@ -1,12 +1,26 @@
 class LanguageModel {
-  final String image;
-  final String code;
-  final String language;
+  String image;
+  String code;
+  String language;
 
   LanguageModel(
       {required this.code, required this.image, required this.language});
 
   factory LanguageModel.from() {
     return LanguageModel(code: '', image: '', language: '');
+  }
+
+  void replaceContent(LanguageModel second) {
+    String codeTemp = second.code;
+    second.code = code;
+    code = codeTemp;
+
+    String languageTemp = second.language;
+    second.language = language;
+    language = languageTemp;
+
+    String imageTemp = second.image;
+    second.image = image;
+    image = imageTemp;
   }
 }
