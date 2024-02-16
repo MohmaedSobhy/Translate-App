be660d8294beb0c74fe93f6043c4fb0e72bc90b1
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-02-16 10:19:42 +0200

    refactor name for each country to countryName+flage

--- lib/core/utils/app_assest.dart
+++ lib/core/utils/app_assest.dart
@@ -3,10 +3,10 @@ abstract class AppAssests {
   static const String appLogo = "${base}logo.png";
   static const String egyptFlag = "${base}egypt.png";
   static const String turkeyFlage = "${base}turkey.png";
-  static const String unitedKindom = "${base}united_kingdom.png";
-  static const String argentine = "${base}argentine.png";
-  static const String unitedStates = "${base}united_states.png";
-  static const String indian = "${base}indian.png";
-  static const String russia = "${base}russia.png";
-  static const String germany = "${base}germany.png";
+  static const String unitedKindomFlage = "${base}united_kingdom.png";
+  static const String argentineFlage = "${base}argentine.png";
+  static const String unitedStatesFlage = "${base}united_states.png";
+  static const String indianFlage = "${base}indian.png";
+  static const String russiaFlage = "${base}russia.png";
+  static const String germanyFlage = "${base}germany.png";
 }
