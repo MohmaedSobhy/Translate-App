2d7f330f334f05cac02c601e1d5c39a25558b4a9
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-02-17 15:08:53 +0200

    add two text translate and enter text

--- lib/core/utils/app_string.dart
+++ lib/core/utils/app_string.dart
@@ -3,4 +3,6 @@ abstract class AppString {
   static const String from = "Translate";
   static const String translateOnToGo = "Translate on the Go";
   static const String selectLanguage = 'Select Language';
+  static const String translate = 'Translate';
+  static const String enterTextHere = 'Enter Text Here';
 }
